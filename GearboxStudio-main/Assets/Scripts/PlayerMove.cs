fbb2002c8321c624eb0a18e0c99029356ed87261
Innokentyib <Kesaburokov@gmail.com>
2024-02-17 13:18:16 +0500

    кубик
    починил кубик ну почти

---
+++ GearboxStudio-main/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float jumpForce = 600f;
+    float speed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);  //или transform.position?
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            //rb.AddForce(new Vector2 (0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
