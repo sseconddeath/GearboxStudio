8b427eb69a2e450a5cd485cc8d173e5fe9a62e84
Innokentyib <Kesaburokov@gmail.com>
2024-02-22 22:14:40 +0500

    он поварачивается

--- Assets/Scripts/PlayerMove1.cs
+++ Assets/Scripts/PlayerMove1.cs
@@ -8,6 +8,7 @@ public class PlayerMove : MonoBehaviour
     float jumpForce = 700f;
     float speed = 10f;
     private Vector2 moveVector;
+    private bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,15 @@ public class PlayerMove : MonoBehaviour
         moveVector.y = Input.GetAxis("Vertical");
         rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
         //rb.position + moveVector * speed * Time.deltaTime;
+        if (facingRight == false && moveVector.x > 0)//если игрок смотрит влево поворачивается
+        {
+            Flip();
+         }
 
+       else if (facingRight == true && moveVector.x < 0)//если игрок смотрит вправо поворачивается
+        {
+            Flip();
+        }
 
         //float moveX = Input.GetAxis("Horizontal");
         //rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
@@ -31,5 +40,20 @@ public class PlayerMove : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce);
             //rb.AddForce(new Vector2 (0, jumpForce), ForceMode2D.Impulse);
         }
+
+
+
+
+    }
+
+
+
+
+    void Flip() //поворот персонажа
+    { facingRight = !facingRight;
+    Vector3 scaler = transform.localScale;
+        scaler.x  *= -1;
+        transform.localScale = scaler; 
+    
     }
 }
