4ef2f64776250f17e938a1b25fba3c041cdbd514
Innokentyib <Kesaburokov@gmail.com>
2024-03-09 01:13:30 +0500

    пытался написать уничтожение пули (не работает)

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
     float moveSpeed = 7f;
     Rigidbody2D rb;
     PlayerMove target;
+    public float timeDestroy = 3f;
     Vector2 moveDirection;
     void Start()
     {
@@ -15,13 +16,15 @@ public class Bullet : MonoBehaviour
         moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
         rb.velocity = new Vector2(moveDirection.x, 0/*moveDirection.y*/);
         Destroy(gameObject, 3f);
+        Invoke("DestroyBullet",timeDestroy);
+       
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            
         }
     }
 
