511211e0231fd949144a959cd2257de773e9de70
Sha1tan01 <nurbakiev2001@yandex.ru>
2024-03-21 18:36:48 +0500

    Меню

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void ChangeScenes(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
